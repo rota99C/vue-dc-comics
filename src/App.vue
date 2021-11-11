20b66b41272eff9128c2cbc98633511d0fcbd9cf
rota99C <rotaboolean@gmail.com>
2021-11-11 17:46:04 +0100

    header completo

--- src/App.vue
+++ src/App.vue
@@ -27,5 +27,9 @@ export default {
 </script>
 
 <style lang="scss">
-
+ * {
+   padding: 0;
+   margin: 0;
+   box-sizing: border-box;
+ }
 </style>
--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,7 +1,22 @@
 <template>
    <header id="site-header">
-      <div class="logo">logo</div>
-      <div class="navbar">navbar</div>
+      <div class="logo">
+          <img src="../assets/img/dc-logo.png" alt="">
+      </div>
+      <div class="navbar">
+          <ul>
+              <li><a>CHARACTERS</a></li>
+              <li><a class="active">COMICS</a></li>
+              <li><a>MOVIES</a></li>
+              <li><a>TV</a></li>
+              <li><a>COLLECTIBILES</a></li>
+              <li><a>GAMES</a></li>
+              <li><a>VIDEOS</a></li>
+              <li><a>FANS</a></li>
+              <li><a>NEWS</a></li><a></a>
+              <li><a>SHOP</a></li>
+          </ul>
+      </div>
     </header>
 </template>
 
@@ -12,23 +27,49 @@ export default {
 </script>
 
 <style lang= "scss">
+
+@import url('https://fonts.googleapis.com/css2?family=Acme&display=swap');
+/* @import url('../assets/variables.scss'); */
+
 #site-header {
-    width: 960px;
+    width: 1000px;
     height: 90px;
     margin: auto;
     display: flex;
     justify-content: space-between;
-    background-color: grey;
     
         .logo {
             width: 90px;
             height: 90px;
-            background-color: green;
+            padding: 10px;
+
+            img {
+                width: 100%;
+                height: 100%;
+            }
         }
         .navbar {
-            width: 550px;
+            width: 590px;
             height: 90px;
-            background-color: yellow
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+            font-size: 0.8em;
+            font-family: 'Acme', sans-serif;
+        
+        ul {
+
+            li {
+                list-style-type: none;
+                display: inline-block;
+                margin: 0px 8px;
+
+                a.active {
+                    /* color: $brand-blue;  come importare foglio variabili scss? */ 
+                    color: #0282F9;
+                }
+            }
+        }
 
         }
 
