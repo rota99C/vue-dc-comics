82f3ba5a94bd34bc28ddfcbfe6a04da8dd080b00
rota99C <rotaboolean@gmail.com>
2021-11-11 15:44:17 +0100

    blocco colorato header completo

---
+++ vue-dc-comics/src/components/SiteHeader.vue
@@ -0,0 +1,38 @@
+<template>
+   <header id="site-header">
+      <div class="logo">logo</div>
+      <div class="navbar">navbar</div>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang= "scss">
+#site-header {
+    width: 960px;
+    height: 90px;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    background-color: grey;
+        .logo {
+            width: 90px;
+            height: 90px;
+            background-color: green;
+        }
+        .navbar {
+            width: 550px;
+            height: 90px;
+            background-color: yellow
+
+        }
+
+
+
+}
+
+</style>
